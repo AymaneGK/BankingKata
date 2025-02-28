7ba2aeda35ec801d534e7af615f1131027906090
AymaneGK <aymane.matrane@etu.uae.ac.ma>
2025-02-28 03:54:53 +0000

    added a missing comment

--- src/test/java/requirements/TodayDateProviderTest.java
+++ src/test/java/requirements/TodayDateProviderTest.java
@@ -4,12 +4,12 @@ import bankkata.TodayDateProvider;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mock;
-
+//This test will mostlikely fail due to the date changing each day
 public class TodayDateProviderTest {
     @Test
     public void provideTodayDate(){
         TodayDateProvider todayDateProvider = new TodayDateProvider();
         String today = todayDateProvider.todayString();
-        Assert.assertEquals("27/02/2025",today); // Make sure to change it when reviewing my code
+        Assert.assertEquals("28/02/2025",today); // Make sure to change it when reviewing my code
     }
 }
